d9097ea528edc1709c17c53eeb7b624fff634abe
omniclown <=markascerniauskas@gmail.com>
2019-10-19 22:27:15 +0300

    Kiela not smart

--- Disasters/predictor.py
+++ Disasters/predictor.py
@@ -5,6 +5,7 @@ import pytz
 import urllib
 import pandas as pd
 import io
+from math import sin, cos, sqrt, atan2, radians, sqrt
 
 
 class Earthquake:
@@ -13,14 +14,34 @@ class Earthquake:
         self.latitude = latitude
         self.magnitude = magnitude
         self.time = time
+        self.strain_radius = 10 ** (0.43 * magnitude)
 
-    def get_radius():  # TODO
-        self.radius = 1
+    def distance_between_points(lon1, lon2, lat1, lat2):
+        lon1 = radians(lon1)
+        lon1 = radians(lon2)
+        lat1 = radians(lat1)
+        lat2 = radians(lat2)
+        R = 6373.0
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        distance = R * c
+        return distance
 
-    def debug_message():
+    def debug_message(self):
         print(
-            f"Type: {self.type}\nLongitude: {self.longitude}\nLatitude: {self.latitude}\nTimestamp: {self.time}"
+            f"Longitude: {self.longitude}\nLatitude: {self.latitude}\nTimestamp: {self.time}\nMagnitude: {self.magnitude}\nImpact radius: {self.strain_radius}"
         )
+
+    def user_in_danger(self, users):
+        users_in_danger = []
+        for user in users:
+            if distance_between_points(
+                self.longitude, user.longitude, self.latitude, user.latitude
+            ):
+                users_in_danger.append(user)
+        return users_in_danger
 
 
 def get_earthquake_data(time_in_minutes):
@@ -51,4 +72,16 @@ def get_clean_data(time_in_minutes=60):
     return disasters
 
 
-disasters = get_clean_data()
+def explain_list(disasters):
+    print(f"There was a total of: {len(disasters)}")
+    important_earthquake = []
+    for earthquake in disasters:
+        if earthquake.magnitude >= 3:
+            important_earthquake.append(earthquake)
+
+    print(f"There was a total of: {len(important_earthquake)} with magnitude >= 3")
+    print(disasters[0].debug_message())
+
+
+disasters = get_clean_data(1440)
+explain_list(disasters)
